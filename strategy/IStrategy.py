c540aa549abb73befa7a83d1ff643e729ae3cf4c
McanKul <kul3562@gmail.com>
2025-11-25 16:42:27 +0300

    feat(Istrategy): Istraetegy generate signals and live sginalss are added, live signals are added

---
+++ strategy/IStrategy.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+from typing import Optional
+
+#abstract class for strategies
+class IStrategy(ABC):
+   #signal generation
+   @abstractmethod
+   def generate_signal(self, symbol: str) -> Optional[str]: pass
+   #multi signal generations
+   @staticmethod
+   @abstractmethod
+   def generate_signals(df: pd.DataFrame) -> pd.Series: pass
+
+   #for future devolopment
+   # @abstractmethod
+   # def sl_pct(self) -> float: pass
+   
+   # @abstractmethod
+   # def tp_pct(self) -> float: pass
